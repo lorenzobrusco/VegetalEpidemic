73ae2788427f20f91f833b9cdcde04d4266019dd
lorenzobrusco <lorenzo.brusco@libero.it>
2018-03-12 10:28:46 +0100

    fix bugs

--- VegetalEpidemic/VegetalEpidemic.cpp
+++ VegetalEpidemic/VegetalEpidemic.cpp
@@ -3,7 +3,7 @@
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Vegetal epidemic in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
